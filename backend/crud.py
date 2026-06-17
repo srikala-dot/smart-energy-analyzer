84d3353acc7334d4f7d35134f498c24a19874cd3
srikala-dot <kalanaidu678@gmail.com>
2026-06-17 09:32:41 +0530

    Refactor create_dataset and delete_dataset functions

--- backend/crud.py
+++ backend/crud.py
@@ -4,17 +4,28 @@ from . import models
 def get_datasets(db: Session):
     return db.query(models.Dataset).all()
 
-def create_dataset(db: Session, filename: str, path: str):
-    new_entry = models.Dataset(filename=filename, filepath=path)
-    db.add(new_entry)
+def create_dataset(db: Session, filename: str, filepath: str):
+    new_dataset = models.Dataset(
+        filename=filename,
+        filepath=filepath
+    )
+
+    db.add(new_dataset)
     db.commit()
-    db.refresh(new_entry)
-    return new_entry
+    db.refresh(new_dataset)
+
+    return new_dataset
 
 def delete_dataset(db: Session, dataset_id: int):
-    entry = db.query(models.Dataset).filter(models.Dataset.id == dataset_id).first()
-    if entry:
-        db.delete(entry)
+    dataset = (
+        db.query(models.Dataset)
+        .filter(models.Dataset.id == dataset_id)
+        .first()
+    )
+
+    if dataset:
+        db.delete(dataset)
         db.commit()
         return True
+
     return False
